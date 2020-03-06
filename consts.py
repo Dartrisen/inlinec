f1ea4c674cd5a67bd5bbe365af9aada8c697c297
Dartrisen <calculating.scientist@gmail.com>
2020-03-06 15:16:06 +0100

    update inlinec.py, add consts.py

---
+++ consts.py
@@ -0,0 +1,13 @@
+from ctypes import c_char, c_int, c_float, c_double, c_bool, c_short, c_long, c_size_t
+
+types = {'char'     : c_char,
+         'int'      : c_int,
+         'float'    : c_float,
+         '_Bool'    : c_bool,
+         'double'   : c_double,
+         'short'    : c_short,
+         'long'     : c_long,
+         'size_t'   : c_size_t}
+
+arg_pattern = r'(char|int|float|_Bool|double|short|long|size_t)+ \w+'
+res_pattern = r'(char|int|float|_Bool|double|short|long|size_t)\s'
--- inlinec.py
+++ inlinec.py
@@ -1,15 +1,16 @@
 import os
-from re import search, findall
+from re import search, findall, match
 from shlex import quote
 from atexit import register
 from sys import stderr, exit
 from tempfile import NamedTemporaryFile, mktemp
 from inspect import getfullargspec, formatargspec, getargspec
-from ctypes import cdll, c_char, c_int, c_float, c_double, c_bool, c_short, c_long, c_size_t
+from ctypes import cdll
+from consts import types, arg_pattern, res_pattern
 
 
 try:
-    #os.environ['CC'] = 'clang'
+    os.environ['CC'] = 'clang'
     COMPILER = os.environ['CC']
 except KeyError:
     print('CC environment variable must point to C compiler', file=stderr)
@@ -30,7 +31,8 @@ class C:
     def __call__(self, *args, **kwargs):
         name = self.func.__name__
         defaults = self.defaults or tuple()
-        self.__getitem__(name).argtypes = self._typeparser()
+        self.__getitem__(name).argtypes = self._argparser()
+        self.__getitem__(name).restype = self._resparser()
         args = self._argconstructor(args, kwargs)
         if args == defaults:
             return self.__getitem__(name)(*args)
@@ -38,13 +40,16 @@ class C:
             args = args + defaults
             return self.__getitem__(name)(*args)
 
-    def _typeparser(self):
-        text = search(r'\((.*?)\)', self.func(self.args)).group(1)
-        pattern = r'(char|int|float|_Bool|double|short|long|size_t)+ \w+'
-        text = findall(pattern, text)
-        types = {'char':c_char, 'int':c_int, 'float':c_float, '_Bool':c_bool,
-                 'double':c_double, 'short':c_short, 'long':c_long, 'size_t':c_size_t}
+    def _argparser(self):
+        string = self.func(self.args)
+        text = search(r'\((.*?)\)', string).group(1)
+        text = findall(arg_pattern, text)
         return [types[type_] for type_ in text]
+
+    def _resparser(self):
+        string = self.func(self.args)
+        text = search(res_pattern, string).group(1)
+        return types[text]
 
     def _argconstructor(self, args, kwargs):
         args = args or tuple()
