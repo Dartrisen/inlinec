911ba8722ab4a0a2f16edc488f08ad2efa80558c
Dartrisen <calculating.scientist@gmail.com>
2020-03-06 11:29:50 +0100

    update inlinec.py
    - added typeparser for c code (you could use now char, int, float, double, _Bool, short, long, size_t types)
    - some bugs fixed

--- inlinec.py
+++ inlinec.py
@@ -1,14 +1,15 @@
 import os
-from ctypes import cdll
+from re import search, findall
 from shlex import quote
 from atexit import register
 from sys import stderr, exit
-from inspect import getfullargspec
 from tempfile import NamedTemporaryFile, mktemp
+from inspect import getfullargspec, formatargspec, getargspec
+from ctypes import cdll, c_char, c_int, c_float, c_double, c_bool, c_short, c_long, c_size_t
+
 
 try:
-    # if you're using the clang compiler
-    # os.environ['CC'] = 'clang'
+    os.environ['CC'] = 'clang'
     COMPILER = os.environ['CC']
 except KeyError:
     print('CC environment variable must point to C compiler', file=stderr)
@@ -22,17 +23,34 @@ class C:
     def __init__(self, func, *shared_libs):
         self.func = func
         self.library = None
-        self.args = getfullargspec(func)
+        self.args = formatargspec(*getfullargspec(func))
+        self.defaults = getfullargspec(func).defaults
         self._compile(self.func(self.args), shared_libs)
 
     def __call__(self, *args, **kwargs):
         name = self.func.__name__
-        if args:
+        defaults = self.defaults or tuple()
+        self._typeparser()
+        self.__getitem__(name).argtypes = self._typeparser()
+        args = self._argconstructor(args, kwargs)
+        if args == defaults:
             return self.__getitem__(name)(*args)
-        elif kwargs:
-            return self.__getitem__(name)(*kwargs.values())
         else:
-            return self.__getitem__(name)(*self.args.defaults)
+            args = args + defaults
+            return self.__getitem__(name)(*args)
+
+    def _typeparser(self):
+        text = search(r'\((.*?)\)', self.func(self.args)).group(1)
+        pattern = r'(char|int|float|_Bool|double|short|long|size_t)+ \w+'
+        text = findall(pattern, text)
+        types = {'char':c_char, 'int':c_int, 'float':c_float, '_Bool':c_bool,
+                 'double':c_double, 'short':c_short, 'long':c_long, 'size_t':c_size_t}
+        return [types[type_] for type_ in text]
+
+    def _argconstructor(self, args, kwargs):
+        args = args or tuple()
+        kwargs = tuple(kwargs.values()) or tuple()
+        return args + kwargs
 
     def _compile(self, code, libs):
         with NamedTemporaryFile(mode='w', prefix='PYC', suffix='.c',
